f02a3c8b125920ff1cca79f4c215a78245a92621
anteqkois <koisantek96@gmail.com>
2022-04-23 23:35:02 +0200

    fix(tailwind): change tailwind config to right content

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Navbar = () => {
-  return <nav className="fixed top-0 left-0 flex w-full text-3xl">wededededededede</nav>;
+  return <nav className="fixed top-0 left-0 flex w-full bg-slate-800 text-red-800 text-3xl">wededededededede</nav>;
 };
 
 export default Navbar;
