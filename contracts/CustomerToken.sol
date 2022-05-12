8e74354de58243db1d28c5a0bbb476c804f337d4
anteqkois <koisantek96@gmail.com>
2022-05-12 23:09:26 +0200

    feat(customertoken): start create new version

--- contracts/CustomerToken.sol
+++ contracts/CustomerToken.sol
@@ -9,8 +9,8 @@ contract CustomerToken is Ownable, ICustomerToken {
     string private _symbol;
     uint256 private _totalSupply;
 
-    mapping(address => mapping(address => uint256)) private _allowances;
-    mapping(address => uint256) private _balances;
+    mapping(address => mapping(address => uint256)) private _allowance;
+    mapping(address => uint256) private _balanceOf;
 
     constructor(string memory symbol_, string memory name_)
     // uint256 maxSupply_
@@ -56,7 +56,7 @@ contract CustomerToken is Ownable, ICustomerToken {
         override
         returns (uint256)
     {
-        return _balances[account];
+        return _balanceOf[account];
     }
 
     function allowance(address owner, address spender)
@@ -66,7 +66,131 @@ contract CustomerToken is Ownable, ICustomerToken {
         override
         returns (uint256)
     {
-        return _allowances[owner][spender];
+        return _allowance[owner][spender];
+    }
+
+    // function approve(address spender, uint256 amount)
+    //     external
+    //     virtual
+    //     override
+    //     returns (bool)
+    // {
+    //     _approve(msg.sender, spender, amount);
+    //     return true;
+    // }
+
+    function transferFrom(
+        address from,
+        address to,
+        uint256 amount
+    ) external override returns (bool) {
+        uint256 allowed = _allowance[from][msg.sender];
+
+        // ???
+        if (allowed != type(uint256).max)
+            _allowance[from][msg.sender] = allowed - amount;
+
+        _balanceOf[from] -= amount;
+
+        unchecked {
+            _balanceOf[to] += amount;
+        }
+
+        emit Transfer(from, to, amount);
+
+        return true;
+    }
+
+    // function transferFrom(
+    //     address from,
+    //     address to,
+    //     uint256 amount
+    // ) external virtual override returns (bool) {
+    //     // address spender = msg.sender;
+    //     // _spendAllowance(from, spender, amount);
+
+    //     require(from != address(0), "ERC20: transfer from the zero address");
+    //     require(to != address(0), "ERC20: transfer to the zero address");
+
+    //     uint256 fromBalance = _balanceOf[from];
+    //     require(
+    //         fromBalance >= amount,
+    //         "ERC20: transfer amount exceeds balance"
+    //     );
+    //     unchecked {
+    //         _balanceOf[msg.sender] = fromBalance - amount;
+    //     }
+    //     _balanceOf[to] += amount;
+
+    //     emit Transfer(msg.sender, to, amount);
+
+    //     // transfer(to, amount);
+    //     return true;
+    // }
+
+    function transfer(address to, uint256 amount)
+        external
+        override
+        returns (bool)
+    {
+        _balanceOf[msg.sender] -= amount;
+
+        unchecked {
+            _balanceOf[to] += amount;
+        }
+
+        emit Transfer(msg.sender, to, amount);
+
+        return true;
+    }
+
+    // function transfer(
+    //     // address from,
+    //     address to,
+    //     uint256 amount
+    // ) external {
+    //     require(
+    //         msg.sender != address(0),
+    //         "ERC20: transfer msg.sender the zero address"
+    //     );
+    //     require(to != address(0), "ERC20: transfer to the zero address");
+
+    //     uint256 fromBalance = _balanceOf[msg.sender];
+    //     require(
+    //         fromBalance >= amount,
+    //         "ERC20: transfer amount exceeds balance"
+    //     );
+    //     unchecked {
+    //         _balanceOf[msg.sender] = fromBalance - amount;
+    //     }
+    //     _balanceOf[to] += amount;
+
+    //     emit Transfer(msg.sender, to, amount);
+    // }
+
+    // switch after to internal, in Factory pattern
+    function mint(address account, uint256 amount) external onlyOwner {
+        // nie będzie możliwości by wywołąć tą funkcję do mintowani do adresu 0
+        // require(account != address(0), "ERC20: mint to the zero address");
+
+        unchecked {
+            _balanceOf[account] += amount;
+            _totalSupply += amount;
+        }
+        emit Transfer(address(0), account, amount);
+    }
+
+    function burn(address account, uint256 amount) external {
+        require(account != address(0), "ERC20: burn from the zero address");
+
+        uint256 accountBalance = _balanceOf[account];
+        require(accountBalance >= amount, "ERC20: burn amount exceeds balance");
+        unchecked {
+            _balanceOf[account] = accountBalance - amount;
+            _totalSupply -= amount;
+        }
+
+        emit Transfer(account, address(0), amount);
     }
 
     function approve(address spender, uint256 amount)
@@ -75,108 +199,10 @@ contract CustomerToken is Ownable, ICustomerToken {
         override
         returns (bool)
     {
-        // address owner = msg.sender;
-        _approve(msg.sender, spender, amount);
-        return true;
-    }
-
-    function transferFrom(
-        address from,
-        address to,
-        uint256 amount
-    ) external virtual override returns (bool) {
-        address spender = msg.sender;
-        _spendAllowance(from, spender, amount);
-        transfer(to, amount);
-        return true;
-    }
-
-    // function transfer(address to, uint256 amount)
-    //     external
-    //     virtual
-    //     override
-    //     returns (bool)
-    // {
-    //     // address owner = msg.sender;
-    //     _transfer(msg.sender, to, amount);
-    //     return true;
-    // }
-
-    function transfer(
-        // address from,
-        address to,
-        uint256 amount
-    ) internal {
-        require(
-            msg.sender != address(0),
-            "ERC20: transfer msg.sender the zero address"
-        );
-        require(to != address(0), "ERC20: transfer to the zero address");
-
-        uint256 fromBalance = _balances[msg.sender];
-        require(
-            fromBalance >= amount,
-            "ERC20: transfer amount exceeds balance"
-        );
-        unchecked {
-            _balances[msg.sender] = fromBalance - amount;
-        }
-        _balances[to] += amount;
-
-        emit Transfer(msg.sender, to, amount);
-    }
-
-    // switch after to internal, in Factory pattern
-    function mint(address account, uint256 amount) external onlyOwner {
-        // nie będzie możliwości by wywołąć tą funkcję do mintowani do adresu 0
-        // require(account != address(0), "ERC20: mint to the zero address");
-
-        unchecked {
-            _balances[account] += amount;
-            _totalSupply += amount;
-        }
-        emit Transfer(address(0), account, amount);
-    }
-
-    function _burn(address account, uint256 amount) internal {
-        require(account != address(0), "ERC20: burn from the zero address");
-
-        uint256 accountBalance = _balances[account];
-        require(accountBalance >= amount, "ERC20: burn amount exceeds balance");
-        unchecked {
-            _balances[account] = accountBalance - amount;
-            _totalSupply -= amount;
-        }
-
-        emit Transfer(account, address(0), amount);
-    }
-
-    function _approve(
-        address owner,
-        address spender,
-        uint256 amount
-    ) internal {
-        require(owner != address(0), "ERC20: approve from the zero address");
         require(spender != address(0), "ERC20: approve to the zero address");
 
-        _allowances[owner][spender] = amount;
-        emit Approval(owner, spender, amount);
-    }
-
-    function _spendAllowance(
-        address owner,
-        address spender,
-        uint256 amount
-    ) internal {
-        uint256 currentAllowance = _allowances[owner][spender];
-        if (currentAllowance != type(uint256).max) {
-            require(
-                currentAllowance >= amount,
-                "ERC20: insufficient allowance"
-            );
-            unchecked {
-                _approve(owner, spender, currentAllowance - amount);
-            }
-        }
+        _allowance[msg.sender][spender] = amount;
+        emit Approval(msg.sender, spender, amount);
+        return true;
     }
 }
