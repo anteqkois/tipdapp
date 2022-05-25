86e26d81d39d1ec3e1f15f1459766a72d40525a6
anteqkois <koisantek96@gmail.com>
2022-05-25 09:30:56 +0200

    feat(theme): add outline to input

--- src/components/utils/Input.jsx
+++ src/components/utils/Input.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Input = ({ type, id, name, label, placeholder, onChange, value, error }) => {
   return (
     <div className="my-3">
-      <label htmlFor={label} className="block mb-2 text-sm font-medium text-gray-900">
+      <label htmlFor={label} className="block mb-2 text-sm font-medium text-neutral-900">
         {label}
       </label>
       <input
@@ -13,9 +13,9 @@ const Input = ({ type, id, name, label, placeholder, onChange, value, error }) =
         onChange={onChange}
         value={value}
         placeholder={placeholder}
-        className={`block p-2 w-full text-gray-900 bg-gray-50 rounded-lg border ${
-          error ? 'border-red-600' : 'border-gray-300'
-        } focus:ring-blue-500 focus:border-blue-500`}
+        className={`block p-2 w-full text-neutral-900 bg-gray-50 rounded-lg border ${
+          error ? 'border-red-600' : 'border-neutral-300'
+        } outline-2 outline-offset-2 focus:outline-primary-300 focus:border-primary-800`}
       ></input>
       <p className="text-red-600 min-h-[24px]">{error && `* ${error}`}</p>
     </div>
