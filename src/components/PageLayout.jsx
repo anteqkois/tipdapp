1ce71f0686e62263da971a099a77293df2b8ba7e
anteqkois <koisantek96@gmail.com>
2022-08-22 13:14:05 +0200

    style(navigation): instal heroicons, change icon on mobile navigation view

--- src/components/PageLayout.jsx
+++ src/components/PageLayout.jsx
@@ -1,9 +1,7 @@
-import React from 'react';
-
 const PageLayout = ({ children }) => {
   return (
     <>
-      <main className="max-w-6xl p-2 mx-auto mt-12 lg:p-8 lg:mt-24">{children}</main>
+      <main className="">{children}</main>
     </>
   );
 };
--- src/services/Web3Provider.jsx
+++ src/services/Web3Provider.jsx
@@ -1,3 +1,4 @@
+import useMediaQuery from '@/hooks/useMediaQuery';
 import { getDefaultWallets, RainbowKitProvider } from '@rainbow-me/rainbowkit';
 import '@rainbow-me/rainbowkit/styles.css';
 import { Toaster } from 'react-hot-toast';
@@ -6,39 +7,41 @@ import { jsonRpcProvider } from 'wagmi/providers/jsonRpc';
 import { publicProvider } from 'wagmi/providers/public';
 
 const { chains, provider } = configureChains(
-    [chain.mainnet, chain.hardhat, chain.polygon, chain.optimism, chain.arbitrum],
-    [
-        publicProvider(),
-        jsonRpcProvider({
-            rpc: (chain) => ({
-                http: `http://127.0.0.1:8545/`,
-                webSocket: `wss://127.0.0.1:8545/`,
-            }),
-        }),
-        // alchemyProvider({ apiKey: process.env.ALCHEMY_API_KEY }),
-    ],
+  [chain.mainnet, chain.hardhat, chain.polygon, chain.optimism, chain.arbitrum],
+  [
+    publicProvider(),
+    jsonRpcProvider({
+      rpc: (chain) => ({
+        http: `http://127.0.0.1:8545/`,
+        webSocket: `wss://127.0.0.1:8545/`,
+      }),
+    }),
+    // alchemyProvider({ apiKey: process.env.ALCHEMY_API_KEY }),
+  ],
 );
 
 const { connectors } = getDefaultWallets({
-    appName: 'My RainbowKit App',
-    chains,
+  appName: 'My RainbowKit App',
+  chains,
 });
 
 const wagmiClient = createClient({
-    // autoConnect: true,
-    connectors,
-    provider,
+  // autoConnect: true,
+  connectors,
+  provider,
 });
 
 const Web3Provider = ({ children }) => {
-    return (
-        <WagmiConfig client={wagmiClient}>
-            <RainbowKitProvider chains={chains}>
-                <Toaster position="top-center" reverseOrder={false} />
-                {children}
-            </RainbowKitProvider>
-        </WagmiConfig>
-    );
+  const isMobile = useMediaQuery('(max-width: 1024px)', true);
+
+  return (
+    <WagmiConfig client={wagmiClient}>
+      <RainbowKitProvider chains={chains} modalSize={isMobile ? 'compact' : 'wide'}>
+        <Toaster position="top-center" reverseOrder={false} />
+        {children}
+      </RainbowKitProvider>
+    </WagmiConfig>
+  );
 };
 
 export default Web3Provider;
