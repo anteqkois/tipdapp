1a0dc4f0e7d8f14f1c2632ea7dc02c4872aee032
anteqkois <anteqkois.dev@gmail.com>
2022-12-25 10:57:55 +0100

    test(contracts): above 98% of the code covered by test

--- contracts/contracts/Tipdapp/ChailinkPriceFeeds.sol
+++ contracts/contracts/Tipdapp/ChailinkPriceFeeds.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8;
 
-// import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 import "./interfaces/AggregatorV3Interface.sol";
 
 contract ChailinkPriceFeeds {
@@ -11,14 +10,5 @@ contract ChailinkPriceFeeds {
         AggregatorV3Interface oracle = AggregatorV3Interface(_addressTokenFeeds);
         (, int256 price, , , ) = oracle.latestRoundData();
         return uint256(price * 10 ** 10);
-    }
-
-    function scalePrice(int256 _price, uint8 _priceDecimals, uint8 _decimals) internal pure returns (int256) {
-        if (_priceDecimals < _decimals) {
-            return _price * int256(10 ** uint256(_decimals - _priceDecimals));
-        } else if (_priceDecimals > _decimals) {
-            return _price / int256(10 ** uint256(_priceDecimals - _decimals));
-        }
-        return _price;
     }
 }
